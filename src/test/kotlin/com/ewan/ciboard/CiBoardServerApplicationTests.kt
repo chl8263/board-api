8c496ecfb93fafc77b12dee064858b1c35bd4e63
Ewan-desktop <chl8263@naver.com>
2021-09-21 16:32:41 +0900

    Add h2 database for test code

--- src/test/kotlin/com/ewan/ciboard/CiBoardServerApplicationTests.kt
+++ src/test/kotlin/com/ewan/ciboard/CiBoardServerApplicationTests.kt
@@ -2,8 +2,10 @@ package com.ewan.ciboard
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CiBoardServerApplicationTests {
 
     @Test
