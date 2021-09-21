79da7b380a6a0f5cc858a3fcbb5b966a88a0911e
Ewan-desktop <chl8263@naver.com>
2021-09-21 15:25:58 +0900

    Fix main file name --> Application.class

--- src/main/kotlin/com/ewan/ciboard/Application.kt
+++ src/main/kotlin/com/ewan/ciboard/Application.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CiBoardServerApplication
+class Application
 
 fun main(args: Array<String>) {
     runApplication<CiBoardServerApplication>(*args)
